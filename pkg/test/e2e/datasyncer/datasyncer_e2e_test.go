19973ac7ad914d8de73338fcecc2ac673ce31196
Mohamed Rafraf <81432497+mohamed-rafraf@users.noreply.github.com>
2025-09-28 00:02:29 +0100

    Add DataSyncer Controller + E2E tests (#27)

---
+++ pkg/test/e2e/datasyncer/datasyncer_e2e_test.go
@@ -0,0 +1,231 @@
+//go:build e2e
+// +build e2e
+
+/*
+Copyright 2025 The KubeFlag Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package datasyncer
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/kubeflag/kubeflag/pkg/controllers/challenge"
+	datasyncercontroller "github.com/kubeflag/kubeflag/pkg/controllers/datasyncer"
+	kubeflaglog "github.com/kubeflag/kubeflag/pkg/log"
+	"github.com/kubeflag/kubeflag/pkg/test/e2e/suite"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+var (
+	ctx       context.Context
+	cancel    context.CancelFunc
+	k8sClient client.Client
+	mgr       manager.Manager
+	cfg       *rest.Config
+	log       = kubeflaglog.NewDefault()
+)
+
+// Ginkgo entry point
+func TestDataSyncerE2E(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "DataSyncer Controller E2E Suite")
+}
+
+// Bootstraps a real manager and client against the Kind cluster.
+var _ = BeforeSuite(func() {
+	mgr, ctx, cancel = suite.Bootstrap()
+
+	// Run controller inside a goroutine.
+	go func() {
+		defer GinkgoRecover()
+		Expect(datasyncercontroller.Add(ctx, mgr, 1, &log)).To(Succeed())
+		Expect(mgr.Start(ctx)).To(Succeed())
+	}()
+
+	k8sClient = mgr.GetClient()
+	Expect(k8sClient).NotTo(BeNil())
+})
+
+var _ = AfterSuite(func() {
+	suite.Teardown(cancel)
+})
+
+// newTokenSecret creates a Kubernetes Secret with token data and annotation
+func newTokenSecret(name, challengeName string) *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default",
+			Annotations: map[string]string{
+				challenge.DataObjectAnnotationKey: fmt.Sprintf(`["%s"]`, challengeName),
+			},
+		},
+		Type: corev1.SecretTypeOpaque,
+		Data: map[string][]byte{
+			"token": []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+		},
+	}
+}
+
+func newConfigMap(name, challengeName string) *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default", // or the source namespace
+			Annotations: map[string]string{
+				challenge.DataObjectAnnotationKey: fmt.Sprintf(`["%s"]`, challengeName),
+			},
+		},
+		Data: map[string]string{
+			"token": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		},
+	}
+}
+
+var _ = Describe("Secret Synchronization E2E", func() {
+	const (
+		timeout  = 30 * time.Second
+		interval = 2 * time.Second
+	)
+
+	It("keeps the synced Secret updated", func() {
+		name := fmt.Sprintf("secret-%d", time.Now().UnixNano())
+		challengeName := fmt.Sprintf("chal-%s", name)
+
+		secret := newTokenSecret(name, challengeName)
+		ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: challengeName}}
+
+		By("creating the Challenge namespace")
+		Expect(k8sClient.Create(ctx, ns)).To(Succeed())
+		DeferCleanup(func() {
+			_ = k8sClient.Delete(ctx, secret)
+			_ = k8sClient.Delete(ctx, ns)
+		})
+
+		By("waiting for namespace to be Active")
+		Eventually(func() (corev1.NamespacePhase, error) {
+			var got corev1.Namespace
+			if err := k8sClient.Get(ctx, client.ObjectKey{Name: challengeName}, &got); err != nil {
+				return "", err
+			}
+			return got.Status.Phase, nil
+		}, timeout, interval).Should(Equal(corev1.NamespaceActive))
+
+		By("creating the source Secret")
+		Expect(k8sClient.Create(ctx, secret)).To(Succeed())
+
+		By("verifying the Secret is replicated")
+		Eventually(func() (string, error) {
+			var sec corev1.Secret
+			if err := k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &sec); err != nil {
+				return "", err
+			}
+			return string(sec.Data["token"]), nil
+		}, timeout, interval).Should(Equal("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+		By("updating the source Secret")
+		secret.Data["token"] = []byte("1234567890")
+		Expect(k8sClient.Update(ctx, secret)).To(Succeed())
+
+		By("verifying the update is synced")
+		Eventually(func() (string, error) {
+			var sec corev1.Secret
+			if err := k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &sec); err != nil {
+				return "", err
+			}
+			return string(sec.Data["token"]), nil
+		}, timeout, interval).Should(Equal("1234567890"))
+	})
+})
+
+var _ = Describe("ConfigMap Synchronization E2E", func() {
+	const (
+		timeout  = 30 * time.Second
+		interval = 2 * time.Second
+	)
+
+	It("Normal Sync: Create + Update", func() {
+		name := fmt.Sprintf("configmap-%d", time.Now().UnixNano())
+		challengeName := fmt.Sprintf("chal-for-%s", name)
+
+		cm := newConfigMap(name, challengeName)
+
+		ns := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: challengeName,
+			},
+		}
+
+		By("creating the Challenge namespace")
+		Expect(k8sClient.Create(ctx, ns)).To(Succeed())
+		DeferCleanup(func() {
+			_ = k8sClient.Delete(ctx, cm)
+			_ = k8sClient.Delete(ctx, ns)
+		})
+
+		By("waiting for the namespace to be Active")
+		Eventually(func() (corev1.NamespacePhase, error) {
+			var got corev1.Namespace
+			if err := k8sClient.Get(ctx, client.ObjectKey{Name: challengeName}, &got); err != nil {
+				return "", err
+			}
+			return got.Status.Phase, nil
+		}, timeout, interval).Should(Equal(corev1.NamespaceActive))
+
+		By("creating the source ConfigMap")
+		Expect(k8sClient.Create(ctx, cm)).To(Succeed())
+		By("waiting for the ConfigMap to be synced")
+		Eventually(func() (string, error) {
+			var synced corev1.ConfigMap
+			if err := k8sClient.Get(
+				ctx,
+				client.ObjectKey{Name: name, Namespace: challengeName},
+				&synced,
+			); err != nil {
+				return "", err
+			}
+			return synced.Data["token"], nil
+		}, timeout, interval).Should(Equal("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+		By("updating the ConfigMap")
+		cm.Data["token"] = "1234567890"
+		Expect(k8sClient.Update(ctx, cm)).To(Succeed())
+
+		By("waiting for the updated ConfigMap to be synced")
+		Eventually(func() (string, error) {
+			var synced corev1.ConfigMap
+			if err := k8sClient.Get(
+				ctx,
+				client.ObjectKey{Name: name, Namespace: challengeName},
+				&synced,
+			); err != nil {
+				return "", err
+			}
+			return synced.Data["token"], nil
+		}, timeout, interval).Should(Equal("1234567890"))
+	})
+})
