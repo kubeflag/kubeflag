10e660d5f89eaf700957f431ac1a0fc5502d203a
Mohamed Rafraf <81432497+mohamed-rafraf@users.noreply.github.com>
2026-02-21 15:59:21 +0100

    Copied Objects: E2E for DataSyncer controller (#47)

--- pkg/test/e2e/datasyncer/datasyncer_e2e_test.go
+++ pkg/test/e2e/datasyncer/datasyncer_e2e_test.go
@@ -34,6 +34,7 @@ import (
 	"github.com/kubeflag/kubeflag/pkg/test/e2e/suite"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -160,6 +161,254 @@ var _ = Describe("Secret Synchronization E2E", func() {
 			}
 			return string(sec.Data["token"]), nil
 		}, timeout, interval).Should(Equal("1234567890"))
+	})
+})
+
+var _ = Describe("Copy Tampered", func() {
+	const (
+		timeout  = 30 * time.Second
+		interval = 2 * time.Second
+	)
+
+	It("reverts manual changes made directly to the synced copy", func() {
+		name := fmt.Sprintf("secret-%d", time.Now().UnixNano())
+		challengeName := fmt.Sprintf("chal-%s", name)
+
+		secret := newTokenSecret(name, challengeName)
+		ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: challengeName}}
+
+		By("creating the Challenge namespace")
+		Expect(k8sClient.Create(ctx, ns)).To(Succeed())
+		DeferCleanup(func() {
+			_ = k8sClient.Delete(ctx, secret)
+			_ = k8sClient.Delete(ctx, ns)
+		})
+
+		By("waiting for namespace to be Active")
+		Eventually(func() (corev1.NamespacePhase, error) {
+			var got corev1.Namespace
+			if err := k8sClient.Get(ctx, client.ObjectKey{Name: challengeName}, &got); err != nil {
+				return "", err
+			}
+			return got.Status.Phase, nil
+		}, timeout, interval).Should(Equal(corev1.NamespaceActive))
+
+		By("creating the source Secret")
+		Expect(k8sClient.Create(ctx, secret)).To(Succeed())
+
+		By("waiting for the copy to appear")
+		Eventually(func() error {
+			var sec corev1.Secret
+			return k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &sec)
+		}, timeout, interval).Should(Succeed())
+
+		By("tampering with the copy's data")
+		var copy corev1.Secret
+		Expect(k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &copy)).To(Succeed())
+		copy.Data["token"] = []byte("tampered-value")
+		Expect(k8sClient.Update(ctx, &copy)).To(Succeed())
+
+		By("verifying the copy is reverted to match the source")
+		Eventually(func() (string, error) {
+			var sec corev1.Secret
+			if err := k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &sec); err != nil {
+				return "", err
+			}
+			return string(sec.Data["token"]), nil
+		}, timeout, interval).Should(Equal("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+	})
+})
+
+var _ = Describe("Copy Deleted", func() {
+	const (
+		timeout  = 30 * time.Second
+		interval = 2 * time.Second
+	)
+
+	It("recreates the synced copy when it is deleted while the source still exists", func() {
+		name := fmt.Sprintf("secret-%d", time.Now().UnixNano())
+		challengeName := fmt.Sprintf("chal-%s", name)
+
+		secret := newTokenSecret(name, challengeName)
+		ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: challengeName}}
+
+		By("creating the Challenge namespace")
+		Expect(k8sClient.Create(ctx, ns)).To(Succeed())
+		DeferCleanup(func() {
+			_ = k8sClient.Delete(ctx, secret)
+			_ = k8sClient.Delete(ctx, ns)
+		})
+
+		By("waiting for namespace to be Active")
+		Eventually(func() (corev1.NamespacePhase, error) {
+			var got corev1.Namespace
+			if err := k8sClient.Get(ctx, client.ObjectKey{Name: challengeName}, &got); err != nil {
+				return "", err
+			}
+			return got.Status.Phase, nil
+		}, timeout, interval).Should(Equal(corev1.NamespaceActive))
+
+		By("creating the source Secret")
+		Expect(k8sClient.Create(ctx, secret)).To(Succeed())
+
+		By("waiting for the copy to appear")
+		Eventually(func() error {
+			var sec corev1.Secret
+			return k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &sec)
+		}, timeout, interval).Should(Succeed())
+
+		By("deleting the copy")
+		var copy corev1.Secret
+		Expect(k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &copy)).To(Succeed())
+		Expect(k8sClient.Delete(ctx, &copy)).To(Succeed())
+
+		// Note: We don't want to wait for the copy to be fully removed before checking for recreation, because if the controller is working correctly it may never actually be fully removed (it may be recreated faster than we can observe the deletion). Instead, we can directly check for the presence of the copy after deletion, and if the controller is working it should be present again very quickly.
+		// By("waiting for the copy to be fully removed")
+		// Eventually(func() bool {
+		// 	var sec corev1.Secret
+		// 	return apierrors.IsNotFound(k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &sec))
+		// }, timeout, interval).Should(BeTrue())
+
+		By("verifying the controller recreates the copy")
+		Eventually(func() error {
+			var sec corev1.Secret
+			return k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &sec)
+		}, timeout, interval).Should(Succeed())
+	})
+})
+
+var _ = Describe("Namespace Removed From Annotation", func() {
+	const (
+		timeout  = 30 * time.Second
+		interval = 2 * time.Second
+	)
+
+	It("deletes the copy in a namespace that was removed from the source annotation", func() {
+		name := fmt.Sprintf("secret-%d", time.Now().UnixNano())
+		challengeA := fmt.Sprintf("chal-a-%s", name)
+		challengeB := fmt.Sprintf("chal-b-%s", name)
+
+		// Source targets two namespaces initially.
+		secret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "default",
+				Annotations: map[string]string{
+					challenge.DataObjectAnnotationKey: fmt.Sprintf(`["%s", "%s"]`, challengeA, challengeB),
+				},
+			},
+			Type: corev1.SecretTypeOpaque,
+			Data: map[string][]byte{"token": []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")},
+		}
+		nsA := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: challengeA}}
+		nsB := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: challengeB}}
+
+		By("creating both Challenge namespaces")
+		Expect(k8sClient.Create(ctx, nsA)).To(Succeed())
+		Expect(k8sClient.Create(ctx, nsB)).To(Succeed())
+		DeferCleanup(func() {
+			_ = k8sClient.Delete(ctx, secret)
+			_ = k8sClient.Delete(ctx, nsA)
+			_ = k8sClient.Delete(ctx, nsB)
+		})
+
+		By("waiting for both namespaces to be Active")
+		for _, ns := range []string{challengeA, challengeB} {
+			ns := ns
+			Eventually(func() (corev1.NamespacePhase, error) {
+				var got corev1.Namespace
+				if err := k8sClient.Get(ctx, client.ObjectKey{Name: ns}, &got); err != nil {
+					return "", err
+				}
+				return got.Status.Phase, nil
+			}, timeout, interval).Should(Equal(corev1.NamespaceActive))
+		}
+
+		By("creating the source Secret targeting both namespaces")
+		Expect(k8sClient.Create(ctx, secret)).To(Succeed())
+
+		By("waiting for copies to appear in both namespaces")
+		for _, ns := range []string{challengeA, challengeB} {
+			ns := ns
+			Eventually(func() error {
+				var sec corev1.Secret
+				return k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: ns}, &sec)
+			}, timeout, interval).Should(Succeed())
+		}
+
+		By("removing challengeB from the source annotation")
+		Expect(k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: "default"}, secret)).To(Succeed())
+		secret.Annotations[challenge.DataObjectAnnotationKey] = fmt.Sprintf(`["%s"]`, challengeA)
+		Expect(k8sClient.Update(ctx, secret)).To(Succeed())
+
+		By("verifying the copy in challengeB is deleted")
+		Eventually(func() bool {
+			var sec corev1.Secret
+			return apierrors.IsNotFound(k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeB}, &sec))
+		}, timeout, interval).Should(BeTrue())
+
+		By("verifying the copy in challengeA still exists")
+		Consistently(func() error {
+			var sec corev1.Secret
+			return k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeA}, &sec)
+		}, 5*time.Second, interval).Should(Succeed())
+	})
+})
+
+var _ = Describe("Source Deleted", func() {
+	const (
+		timeout  = 30 * time.Second
+		interval = 2 * time.Second
+	)
+
+	It("deletes all synced copies when the source object is deleted", func() {
+		name := fmt.Sprintf("secret-%d", time.Now().UnixNano())
+		challengeName := fmt.Sprintf("chal-%s", name)
+
+		secret := newTokenSecret(name, challengeName)
+		ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: challengeName}}
+
+		By("creating the Challenge namespace")
+		Expect(k8sClient.Create(ctx, ns)).To(Succeed())
+		DeferCleanup(func() {
+			// Secret may already be deleted by the test; ignore not-found.
+			_ = k8sClient.Delete(ctx, secret)
+			_ = k8sClient.Delete(ctx, ns)
+		})
+
+		By("waiting for namespace to be Active")
+		Eventually(func() (corev1.NamespacePhase, error) {
+			var got corev1.Namespace
+			if err := k8sClient.Get(ctx, client.ObjectKey{Name: challengeName}, &got); err != nil {
+				return "", err
+			}
+			return got.Status.Phase, nil
+		}, timeout, interval).Should(Equal(corev1.NamespaceActive))
+
+		By("creating the source Secret")
+		Expect(k8sClient.Create(ctx, secret)).To(Succeed())
+
+		By("waiting for the copy to appear")
+		Eventually(func() error {
+			var sec corev1.Secret
+			return k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &sec)
+		}, timeout, interval).Should(Succeed())
+
+		By("deleting the source Secret")
+		Expect(k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: "default"}, secret)).To(Succeed())
+		Expect(k8sClient.Delete(ctx, secret)).To(Succeed())
+
+		By("verifying the source is fully deleted")
+		Eventually(func() bool {
+			var sec corev1.Secret
+			return apierrors.IsNotFound(k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: "default"}, &sec))
+		}, timeout, interval).Should(BeTrue())
+
+		By("verifying the copy is also deleted")
+		Eventually(func() bool {
+			var sec corev1.Secret
+			return apierrors.IsNotFound(k8sClient.Get(ctx, client.ObjectKey{Name: name, Namespace: challengeName}, &sec))
+		}, timeout, interval).Should(BeTrue())
 	})
 })
 
